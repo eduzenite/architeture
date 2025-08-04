3245dd169f30fd5f516763c409f70697b836047a
Eduardo Nascimento <eduardo.nascimento@amarassist.com.br>
2025-08-04 18:13:21 -0300

    update docker

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,6 +18,16 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+Route::get('test', function () {
+    $to = 'eduzenite11@gmail.com';
+
+    Mail::raw('Olá! Esse é um email de teste enviado via MailHog.', function ($message) use ($to) {
+        $message->to($to)->subject('Teste de Email via MailHog');
+    });
+
+    return 'Email enviado!';
 });
 
 require __DIR__.'/auth.php';
