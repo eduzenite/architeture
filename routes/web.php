bcf5bf09c320237bc43db7a8bb9a3b74bc233ccb
Eduardo Nascimento <eduardo.nascimento@amarassist.com.br>
2025-08-21 15:23:31 -0300

    correção no docker

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 101;
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
